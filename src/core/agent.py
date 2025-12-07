8fec120b728fcea20eb9b8b920f9d32829edef46
Mohit <bluesky5april2021@gmail.com>
2025-12-07 15:32:27 +0530

    Added Groq Inference

--- src/core/agent.py
+++ src/core/agent.py
@@ -1,6 +1,6 @@
 from .state import Output
 
-from openai import OpenAI
+from groq import Groq
 from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
 from langchain_openai import ChatOpenAI
 from langchain.agents import create_agent
@@ -13,19 +13,27 @@ load_dotenv()
 API_KEY = os.environ['GROQ_API_KEY']
 BASE_URL = os.environ['GROQ_BASE_URL']
 
+# --------------------------------------------
+# ----------------- TODO ---------------------
+# [DONE][1] Fail Safe: Inference Engines: Openai API, ChatOpenAI , normal request: Returns > json schema + thougts
+# [][2] Fix / Break prompts and Fix Context Updation
+# [][3] Action Manager for planning_agent
+
+
 class Agent:
-  def __init__(self, model:str, agent:str, system_instructions_path:str, tools, managed_agents):
-    self.model = OpenAI(
-        api_key=os.environ["GROQ_API_KEY"],
-        base_url=os.environ["GROQ_BASE_URL"],
+  def __init__(self, model:str, agent:str, system_instructions_path:str, user_instructions_path:str, tools, managed_agents):
+    self.inference_client = Groq(
+      api_key=os.environ["GROQ_API_KEY"],
     )
     self.agent = agent
     self.contents = None
+    self.context = None
     self.system_instructions_template_path = system_instructions_path
     self.tools = tools or {}
     self.managed_agents = managed_agents or {}
     self.feedbacks = {}
     self.execution_iteration = 0
+    self.user_prompt = ""
     
 
   def render_yaml_template(self, file_path, var):
@@ -72,18 +80,26 @@ class Agent:
     }
 
     # Update Context With Feedback ------------ >
-    system_content = self.render_yaml_template(self.system_instructions_template_path, prompt_variables)
-    
+    if self.contents == None:
+      system_content = self.render_yaml_template(self.system_instructions_template_path, prompt_variables)
+      self.context = [
+        {"role": "system","content":system_content},
+        {"role": "user", "content":message or task_from_manager}
+      ]
 
     # Call model with current contents
     try:
-      response = self.model.responses.create(
-        input=system_content,
+      response = self.inference_client.chat.completions.create(
+        input=self.context,
         model="openai/gpt-oss-20b",
       )
-      raw = response.output_text
+
+      # LOG 1: Agent's Call
+      # yield {"type":"ASSISTANT","content":{"Resoning":response.choices[0].message.reasoning,"res":res}}
+
+      raw = response.choices[0].message.content
       if isinstance(raw, str):
-          raw = json.loads(raw)
+        raw = json.loads(raw)
 
       res = Output.model_validate(raw)
 
@@ -91,6 +107,9 @@ class Agent:
       #   [SystemMessage(system_content),HumanMessage(content=f"Task: {message or task_from_manager}")]
       # )
     except Exception as e:
+      # LOG 2: Exception
+      # yield {"type":"ERROR","content":e}
+
       res = Output(
         name = "final_answer",
         arguments = {"error":e}
@@ -99,7 +118,7 @@ class Agent:
     print("EXECUTION :\n\n",res)
 
 
-    if res.name != "final_answer":
+    if res.name not in ["final_answer", "SYSTEM"]:
       
       if res.name in [tool['name'] for tool in self.tools.values()]:
         result = self.tools[res.name]['function'](**res.arguments)
@@ -111,13 +130,18 @@ class Agent:
       else:
         result = "tool / agent not found"
 
-      self.feedbacks.update({
-        self.execution_iteration :
-        {
-          "iteration": self.execution_iteration,
-          "name": res.name,
-          "final_answer": result
-        }
+      # self.feedbacks.update({
+      #   self.execution_iteration :
+      #   {
+      #     "iteration": self.execution_iteration,
+      #     "name": res.name,
+      #     "final_answer": result
+      #   }
+      # })
+
+      self.context.append({
+        "role":"assistant",
+        "content": f"\nIteration [{self.execution_iteration}] : Observation from {res.name} : {result} \n"
       })
 
       self.execution_iteration += 1
